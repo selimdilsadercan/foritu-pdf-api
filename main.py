064b9ed343f4b6c60567c682c0efc6588ceea266
selimdilsad <selimdilsadercan@gmail.com>
2024-10-05 13:07:31 +0300

    fix: hided load_dotenv

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import uvicorn
 
 
 # Supabase yapılandırma bilgileri
-load_dotenv()
+# load_dotenv()
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 SUPABASE_BUCKET = os.environ.get("SUPABASE_BUCKET")
